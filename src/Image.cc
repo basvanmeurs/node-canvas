f1f99529eab981c320921ff087166b12dd4b9b0a
NightYoshi370 <maorninja@outlook.com>
2019-07-07 09:59:54 -0700

    Fix changes

--- src/Image.cc
+++ src/Image.cc
@@ -191,18 +191,19 @@ NAN_METHOD(Image::GetSource){
  */
 NAN_GETTER(Image::GetRawData) {
   Image *img = Nan::ObjectWrap::Unwrap<Image>(info.This());
-  if (img->_surface) {
-    // Return raw ARGB data -- just a memcpy()
-    cairo_surface_t *surface = img->_surface;
-    cairo_surface_flush(surface);
-    const unsigned char *data = cairo_image_surface_get_data(surface);
-    unsigned int nBytes = cairo_image_surface_get_stride(surface) * img->height;
-    Local<Object> buf = Nan::CopyBuffer(reinterpret_cast<const char*>(data), nBytes).ToLocalChecked();
-    info.GetReturnValue().Set(buf);
-  } else {
+
+  if (!img->_surface) {
     info.GetReturnValue().Set(Nan::Undefined());
+    return;
   }
-  return;
+
+  // Return raw ARGB data -- just a memcpy()
+  cairo_surface_t *surface = img->_surface;
+  cairo_surface_flush(surface);
+  const unsigned char *data = cairo_image_surface_get_data(surface);
+  unsigned int nBytes = cairo_image_surface_get_stride(surface) * img->height;
+  Local<Object> buf = Nan::CopyBuffer(reinterpret_cast<const char*>(data), nBytes).ToLocalChecked();
+  info.GetReturnValue().Set(buf);
 }
 
 /*
